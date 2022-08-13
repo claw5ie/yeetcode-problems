014e1ca511c9ba843e7114c8b5e2120c6b2ed07b
claw5ie <92851066+claw5ie@users.noreply.github.com>
2022-08-13 11:23:53 +0100

    Add same-tree.c

---
+++ same-tree.c
@@ -0,0 +1,10 @@
+bool
+isSameTree (struct TreeNode *p, struct TreeNode *q)
+{
+  if (p == NULL && q == NULL)
+    return true;
+  else if ((p != NULL && q != NULL) && p->val == q->val)
+    return isSameTree (p->left, q->left) && isSameTree (p->right, q->right);
+  else
+    return false;
+}
